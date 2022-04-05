c0883e8bea9c1a5ea9df2822316e1eea715335af
mrmc <messifrd7@gmail.com>
2022-04-05 19:51:28 +0430

    Implement Home App, Templates,Static Files

--- core/settings.py
+++ core/settings.py
@@ -40,9 +40,10 @@ INSTALLED_APPS = [
 
     #Extar Apps
     'rest_framework',
-
+    'django.contrib.humanize',
     #Local Apps
     'contact',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- core/urls.py
+++ core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('contact.urls')),
+    path('', include('home.urls'))
 ]
