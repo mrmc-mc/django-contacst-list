9b373d12e8e45d6b205c38aa40419457ee9e1916
mrmc <messifrd7@gmail.com>
2022-04-05 12:47:32 +0430

    Create Models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class DateTimeAbstractModel(models.Model):
+
+        created_at = models.DateTimeField(verbose_name='Creaion Time', auto_now=False, auto_now_add=False)
+
+        class Meta:
+                abstract = True
+
+
+class Contact(DateTimeAbstractModel):
+        
+        fname = models.CharField(verbose_name='First Name', max_length=255)
+        lname = models.CharField(verbose_name='Last Name', max_length=255, blank=True, null=True)
+        phone = models.CharField(verbose_name='Mobile Number',max_length=255)
+        home = models.CharField(verbose_name='Home Number', blank=True, null=True)
+        email = models.EmailField(verbose_name='Email Address', max_length=255,blank=True, null=True)
+
+
+        def __str__(self):
+            return f"{self.fname} {self.lname}"
+        
