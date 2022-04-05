dc6bae1d0aff4c263757a0b2ef21ae64e3f49e40
mrmc <messifrd7@gmail.com>
2022-04-05 12:53:17 +0430

    Create Serializers for Models

---
+++ contact/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+
+        class Meta:
+                model = Contact
+                exclude = ['created_at',]
